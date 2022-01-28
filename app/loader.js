ce1901d24d6f62cef32c1889055bda59bda064cf
TMPLINK STUDIO <45227137+tmplink@users.noreply.github.com>
2022-01-28 23:50:36 +0800

    添加关键组件

--- app/loader.js
+++ app/loader.js
@@ -1,3 +1,7 @@
 class bs5App{
-
+    active(page_path){
+        fetch(page_path).then(response => response.text()).then(html => {
+            document.querySelector('#app').innerHTML = html;
+        });
+    }
 }
